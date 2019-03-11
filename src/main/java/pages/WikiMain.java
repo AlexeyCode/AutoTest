6609821927ed54f302f361006b8c0e082454a67e
erasecorp@hotmail.com <javarushforthosewhoseek>
2019-03-11 13:26:13 +0300

    Initial commit

---
+++ src/main/java/pages/WikiMain.java
@@ -0,0 +1,28 @@
+package pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class WikiMain {
+    WebDriver driver ;
+
+    public WikiMain(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "//input[@name = \"search\"]")
+    WebElement searchButton;
+
+
+    public void sendText(String text){
+        searchButton.sendKeys(text);
+        searchButton.submit();
+    }
+
+
+}
